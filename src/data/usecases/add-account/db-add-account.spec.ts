1ddf12b48ad529851c411b414b73dd4bf6ea398b
Debora Cristina Fontanella <debora.fontanella@barkyn.com>
2022-09-22 09:37:13 +0100

    test: wip test

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -37,4 +37,20 @@ describe('DbAddAccount UseCase', () => {
     await sut.add(accountModel)
     expect(encryptSpy).toHaveBeenLastCalledWith(accountModel.password)
   })
+
+  // test('Should throw if encrypter throws', async () => {
+  //   const { sut, encrypterStub } = makeSut()
+  //   const error = new Error()
+  //   jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise(
+  //     (resolve, reject) => reject(error)
+  //   ))
+
+  //   const accountModel = {
+  //     name: 'valid_name',
+  //     email: 'valid_email@mail.com',
+  //     password: 'valid_password'
+  //   }
+  //   const promise = await sut.add(accountModel)
+  //   await expect(promise).rejects.toThrow()
+  // })
 })
